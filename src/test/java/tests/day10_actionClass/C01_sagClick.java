9e8507bf709d9e9e4faba8bbf00e75a7695a14ff
Your Name <orakomer2551@gmail.com>
2023-10-19 19:32:23 +0300

    raporlu versiyon

--- src/test/java/tests/day10_actionClass/C01_sagClick.java
+++ src/test/java/tests/day10_actionClass/C01_sagClick.java
@@ -1,5 +1,6 @@
 package tests.day10_actionClass;
 
+import com.aventstack.extentreports.ExtentTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -9,17 +10,20 @@ import pages.HerokuAppPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
 
 import java.util.Set;
 
-public class C01_sagClick {
+public class C01_sagClick extends TestBaseRapor {
 
 
     @Test
     public void test01() {
+         extentTest=extentReports.createTest("Alert Testi","Sağ click yapar ve Çıkan alert üzerindeki yazı test edilir ");
         HerokuAppPage herokuAppPage=new HerokuAppPage();
         //2- https://the-internet.herokuapp.com/context_menu sitesine gidin
-       Driver.getDriver().get(ConfigReader.getProperty("herokuAppUrl"));
+       Driver.getDriver().get(ConfigReader.getProperty("herokuAppContextUrl"));
+       extentTest.info("herokuapp.com a gider");
         //3- Cizili alan uzerinde sag click yapin
         Actions actions=new Actions(Driver.getDriver());
         actions.contextClick(herokuAppPage.ciziliAlanElementi).perform();
@@ -28,11 +32,13 @@ public class C01_sagClick {
         String expectedAlertYazi="You selected a context menu";
         String actualAlertYazi=Driver.getDriver().switchTo().alert().getText();
         Assert.assertEquals(expectedAlertYazi,actualAlertYazi);
+        extentTest.pass("Alert üzerindeki yazıyı test eder");
         //5- Tamam diyerek alert’i kapatalim
         Driver.getDriver().switchTo().alert().accept();
         //6- Elemental Selenium linkine tiklayalim
         String ilkSayfaWHD=Driver.getDriver().getWindowHandle();
        herokuAppPage.elementalSeleniumLink.click();
+       extentTest.info("Elemental Selenium linkine tıklar");
         ReusableMethods.bekle(5);
         String ikinciSayfaWHD="";
         Set<String> whd=Driver.getDriver().getWindowHandles();
@@ -50,8 +56,10 @@ public class C01_sagClick {
         String expectedYazi="Make sure your code lands";
         String actualYazi=herokuAppPage.makeSureCodeLandsYaziElementi.getText();
         Assert.assertEquals(expectedYazi,actualYazi);
+        extentTest.pass("Acilan sayfada h1 taginda “Make sure your code lands” yazdigini test eder");
 
         //Sayfalari  kapat(quit)
         Driver.quitDriver();
+        extentTest.info("Sayfaları Kapatır.");
     }
 }
